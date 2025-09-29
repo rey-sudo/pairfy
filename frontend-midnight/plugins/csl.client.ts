a51c849f4a317ee9147714af7d35defe66bd14f1
Juan Rey <sudo.delete.evil@gmail.com>
2025-09-29 17:33:06 -0500

    update

--- frontend-midnight/plugins/csl.client.ts
+++
@@ -1,7 +0,0 @@
-// plugins/csl.client.ts
-import * as CSL from '@emurgo/cardano-serialization-lib-browser'
-
-export default defineNuxtPlugin((nuxtApp) => {
-  if (!import.meta.client) return
-  nuxtApp.provide('CSL', CSL)
-})
