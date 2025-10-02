6a5a52a298399eef0e3f10df5fbb3d685151437b
Juan Rey <sudo.delete.evil@gmail.com>
2025-10-02 12:56:11 -0500

    Update Slider.vue

--- frontend-main/app/components/Slider.vue
+++ frontend-main/app/components/Slider.vue
@@ -9,7 +9,7 @@
   width: 100%;
   height: 360px;
   border-radius: var(--card-radius);
-  background-image: url("https://http2.mlstatic.com/D_NQ_764746-MLA93331780978_092025-OO.webp");
+  background-image: url("https://http2.mlstatic.com/D_NQ_823846-MLA93430362648_092025-OO.webp");
  
 }
 </style>
