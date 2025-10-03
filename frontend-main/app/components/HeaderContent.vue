9747c9d8816158423f381a3956b9ac41d1ffb8dc
Juan Rey <sudo.delete.evil@gmail.com>
2025-10-02 22:33:08 -0500

    update

--- frontend-main/app/components/HeaderContent.vue
+++ frontend-main/app/components/HeaderContent.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="header-content">
-    <Divider vertical transparent />
+    <Logo />
+    <Divider vertical transparent margin="0rem 0.5rem" />
     <Search />
     <Divider vertical transparent margin="0rem 0.5rem" />
     <Button
       label="Connect"
-      backgroundColor="var(--primary-0)" 
+      backgroundColor="var(--primary-0)"
       borderColor="var(--border-1)"
       textColor="var(--text-w)"
     />
---
+++ frontend-main/app/components/Logo.vue
@@ -0,0 +1,7 @@
+<template>
+  <img src="@/assets/icons/logo.svg" alt="" />
+</template>
+
+<script setup></script>
+
+<style lang="css" scoped></style>
