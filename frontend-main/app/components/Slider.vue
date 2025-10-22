1b60d8edb8c9998d98f6d3064d061c4639235fe0
Juan Rey <sudo.delete.evil@gmail.com>
2025-10-22 14:47:07 -0500

    Update Slider.vue

--- frontend-main/app/components/Slider.vue
+++ frontend-main/app/components/Slider.vue
@@ -8,7 +8,7 @@
 .Slider {
   width: 100%;
   height: 360px;
-  background-image: url("https://http2.mlstatic.com/D_NQ_659968-MLA87448511259_072025-OO.webp");
+  background-image: url("https://http2.mlstatic.com/D_NQ_847143-MLA95266072888_102025-OO.webp");
   background-repeat: no-repeat;
   background-position-y: 0%;
   background-size: cover;
