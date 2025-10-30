198343b17c307a762991dff63ef95858a65a5fa4
Juan Rey <sudo.delete.evil@gmail.com>
2025-10-30 12:34:50 -0500

    Update BannerBlackdays.vue

--- frontend-main/app/components/BannerBlackdays.vue
+++ frontend-main/app/components/BannerBlackdays.vue
@@ -2,16 +2,15 @@
   <div class="card">
     <div class="card-header">
       <p>Black Days end in</p>
+      <span class="tag">Until 50% off</span>
     </div>
     <div class="card-content">
-      <BannerCountdown /> 
+      <BannerCountdown />
     </div>
   </div>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
 
 <style scoped>
 .card {
@@ -24,14 +23,23 @@
 .card-header {
   display: flex;
   align-items: center;
+  margin-bottom: 1rem;
   justify-content: space-between;
 }
 
 .card-header p {
   margin: 0;
   font-weight: 700;
-  margin-bottom: 1rem;
   font-size: var(--font-size-2);
+}
+
+.tag {
+  background: rgba(var(--primary-0-base), 0.1);
+  font-size: var(--font-size-1);
+  border-radius: var(--radius-5);
+  color: var(--primary-0);
+  padding: 5px 10px;
+  margin-left: auto;
 }
 
 @media (max-width: 520px) {
